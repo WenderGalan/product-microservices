592548be61592e5f77f256e7c4d663e08d85f047
Wender Galan <wendergalan2014@hotmail.com>
2020-10-29 22:49:20 -0400

    Inicializando o projeto e implementando o CRUD.

--- crud/src/main/java/io/github/wendergalan/crud/exceptions/CrudExceptionHandler.java
+++ crud/src/main/java/io/github/wendergalan/crud/exceptions/CrudExceptionHandler.java
@@ -2,6 +2,7 @@ package io.github.wendergalan.crud.exceptions;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -17,6 +18,7 @@ public class CrudExceptionHandler extends ResponseEntityExceptionHandler {
      * @param request
      * @return
      */
+    @ExceptionHandler(ResourceNotFoundException.class)
     public final ResponseEntity<ExceptionResponse> handlerBadRequestException(Exception ex, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
         return ResponseEntity.badRequest().body(exceptionResponse);
